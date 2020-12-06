17b8ae964c41bdd342820e5da2b67776f0eef213
kgwny <kigawanaoya@gmail.com>
2020-12-06 13:26:28 +0900

    update index.ts

--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,44 @@ import { SheetService } from './sheet.service';
 declare let global: any;
 
 global.createNewFile = (): void => {
-  // パターン１：非同期処理、同期処理の実行
+
+  const hoge = new Promise((resolve) => {
+    const resolveValue = 'resolved1';
+    console.log(`promise1 = ${resolveValue}`);
+    resolve(resolveValue);
+  });
+
+  hoge
+    .catch((error) => {
+      console.log('catch1 : ' + error);
+      return Promise.reject('CATCH1');
+    })
+    .then((success) => {
+      console.log('then : ' + success);
+      return 'THEN';
+    })
+    .catch((error) => {
+      console.log('catch2 : ' + error);
+      throw 'CATCH2';
+    })
+    .catch((error) => {
+      console.log('catch3 : ' + error);
+    });
+
+  const test = new Promise((resolve) => {
+    const resolveValue = 'resolved2';
+    setTimeout(() => {
+      console.log(`promise2 = ${resolveValue}`);
+    }, 1000);
+    resolve(resolveValue);
+  });
+
+  test.then((result) => {
+    console.log(`${result}`);
+  });
+
   asyncTest();
 
-  // パターン２：非同期処理、同期処理の実行
   (async () => {
     console.log(await hidouki('test1'));
     console.log(douki('test2'));
@@ -22,29 +56,16 @@ global.createNewFile = (): void => {
  * 待機させたいときは、同期処理のUtilities.sleepを使用すること
  */
 
-// 非同期処理
-const test = new Promise((resolve) => {
-  const resolveValue = 'resolved1';
-  setTimeout(() => {
-    console.log(`promiseの${resolveValue}`);
-  }, 1000);
-  resolve(resolveValue);
-});
-test.then((result) => {
-  console.log(`thenの${result}`);
-});
-
-// 同期処理
 const promiseTest = (res: number) => {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
       console.log('promiseTest');
       resolve(`promise ${res}`);
+      reject('Error');
     }, 1000);
   });
 };
 
-// パターン１：非同期処理、同期処理
 const asyncTest = async () => {
   try {
     const intentionalErr = 1;
@@ -57,12 +78,10 @@ const asyncTest = async () => {
   }
 };
 
-// 非同期処理
 const hidouki = (hoge: string) => {
   return Promise.resolve(hoge);
 };
 
-// 同期処理
 const douki = (piyo: string) => {
   return piyo;
 };
